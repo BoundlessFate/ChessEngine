51413d16317b15df2c2600b8a8f27948e266b818
Christopher Villanueva <villac3@rpi.edu>
2023-02-16 14:53:54 -0500

    Polished And Commented Customer.h

--- hw/hw4/Customer.h
+++ hw/hw4/Customer.h
@@ -1,18 +1,30 @@
+// Includes string for std::strings and list for std::lists
 #include <string>
 #include <list>
 class Customer {
 public:
+	// Constructor for the customer object
 	Customer(const std::string& aCustomerID, const std::string& aCustomerName);
+	// Returns customer id
 	const std::string& getCustomerID() const {return customerID;}
+	// Returns customer name
 	const std::string& getCustomerName() const {return customerName;}
+	// Returns the rented list
+	// Rented list goes in pairs of:
+	// index 0: id, index 1: num, repeats
 	const std::list<std::string> getRentedList() const {return rentedList;}
-	const std::list<unsigned int> getRentedListNum() const {return rentedListNum;}
+	// Rents the given item and places it in rentedList
 	void rentItem(const std::string& aItemID, unsigned int aNum);
+	// Returns an item and takes it out of the rentedList
+	// Returns the number of items returned
 	unsigned int returnItem(const std::string& aItemID, unsigned int aNum);
+	// Takes in a itemID, and returns a boolean of it is rented
 	bool isRented(const std::string& aItemID);
 private:
+	// Customers id
 	std::string customerID;
+	// Customers name
 	std::string customerName;
+	// list of all current rents
 	std::list<std::string> rentedList;
-	std::list<unsigned int> rentedListNum;
 };
