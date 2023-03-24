552a2cbd4f8b3b8d661da93f6d0ebe5ca51c960c
Christopher Villanueva <villac3@rpi.edu>
2023-03-24 09:43:08 -0400

    Finished homework7

--- hw/hw7/README.txt
+++ hw/hw7/README.txt
@@ -1,7 +1,7 @@
 HOMEWORK 7: SUPER SMASH BROS. FRAMES
 
 
-NAME:  < insert name >
+NAME: Christopher Villanueva
 
 
 COLLABORATORS AND OTHER RESOURCES:
@@ -10,14 +10,14 @@ List the names of everyone you talked to about this assignment
 LMS, etc.), and all of the resources (books, online reference
 material, etc.) you consulted in completing this assignment.
 
-< insert collaborators / resources >
+No collaborators used
 
 Remember: Your implementation for this assignment must be done on your
 own, as described in "Academic Integrity for Homework" handout.
 
 
 
-ESTIMATE OF # OF HOURS SPENT ON THIS ASSIGNMENT:  < insert # hours >
+ESTIMATE OF # OF HOURS SPENT ON THIS ASSIGNMENT: 10
 
 
 
@@ -32,17 +32,18 @@ m: The number of different moves
 t: The highest number of moves that have the same starting frame for a particular move
 
 reading the database/populating your initial data structure:
-
--q (specific move):
-
--q (all):
-
--f:
-
--s:
-
--d:
-
+	m is not used because there are 7 moves, which reduces to O(1) which is negligible time compared to the log time complexities
+-q (specific move): O(log(f) + m)
+	O(log(f)) because that is how long it takes to find the correct fighter in the map and check for a valid move. Everything else is O(1)
+-q (all): O(log(f) + m)
+	O(log(f)) because that is how long it takes to find the correct fighter in the map and check for a valid move. Everything else is O(1)
+-f: O(f(log(f) + m)
+	O(f(log(f)) because all of the indexes have to be added one by one to a set and checked for a valid move, which takes log(f) time for each addition
+-s: O(f(log(f) + m)
+	O(f(log(f)) because the code is virtually identical from -f apart from a different sort function for the set
+-d: O(f(log(f) + m + t)
+	O(f(log(f) + m + t) because the list has to be sorted and then iterated for every move 
+	with the same frame, and also have the move checked to see if it is valid
 
 MISC. COMMENTS TO GRADER:  
 (optional, please be concise!)
--- hw/hw7/main.cpp
+++ hw/hw7/main.cpp
@@ -1,8 +1,9 @@
+// Include statements
 #include <string>
 #include <iostream>
 #include <fstream>
 #include <map>
-#include "Fighter.cpp"
+#include "Fighter.h"
 #include <algorithm>
 #include <set>
 //You may add additional typedefs, includes, etc. up here
@@ -11,17 +12,21 @@
 std::string move_names[7] = {"jab", "forward-tilt", "up-tilt", "down-tilt", "forward-smash", "up-smash", "down-smash"};
 
 //Feel free to put forward declarations here
-
+// code to do the -q action
 void qAction(char*& outputFile, std::map<std::string, Fighter>& data, std::string& wordOne, std::string& wordTwo) {
+	// Create an ofstream for the file to write to
 	std::ofstream outFile;
 	outFile.open(outputFile, std::ios_base::app);
+	// Find the character with that name
 	std::map<std::string, Fighter>::iterator character = data.find(wordOne);
+	// Check for an invalid
 	if (character == data.end()) {
 		outFile << "Invalid character name: " << wordOne << std::endl;
 		outFile << std::endl;
 		outFile.close();
 		return;
 	}
+	// If the file requested for all moves to be outputted
 	if (wordTwo == "all") {
 		outFile << wordOne << " down-smash: " << (character->second).getDsmashTime() << std::endl;
 		outFile << wordOne << " down-tilt: " << (character->second).getDtiltTime() << std::endl;
@@ -31,63 +36,167 @@ void qAction(char*& outputFile, std::map<std::string, Fighter>& data, std::strin
 		outFile << wordOne << " up-smash: " << (character->second).getUsmashTime() << std::endl;
 		outFile << wordOne << " up-tilt: " << (character->second).getUtiltTime() << std::endl;
 		outFile << std::endl;
+		return;
+	}
+	// Find if the move is valid
+	bool validMove = false;
+	for (unsigned int i=0; i < 7; i++) {
+		if (move_names[i] == wordTwo) {
+			validMove = true;
+		}
+	}
+	// If the move isnt valid
+	if (!validMove) {
+		outFile << "Invalid move name: " << wordTwo << std::endl << std::endl;
+		return;
+	}
+	// Output the data for the move from the selected character
+	(character->second).setMove(wordTwo);
+	outFile << wordOne << " " << wordTwo << ": " << (character->second).GetMoveTime()
+		<< std::endl << std::endl;
+	// Close out of the file
+	outFile.close();
+	return;
+}
+// Code for -f request
+void fAction(char*& outputFile, std::map<std::string, Fighter>& data, 
+		std::string& wordOne, std::string& wordTwo) {
+	// Create ofstream for output file
+	std::ofstream outFile;
+	outFile.open(outputFile, std::ios_base::app);
+	// Check if the moves are valid
+	bool validMove = false;
+	for (unsigned int i=0; i < 7; i++) {
+		if (move_names[i] == wordOne) {
+			validMove = true;
+		}
+	}
+	// If not valid, say so in the output file
+	if (!validMove) {
+		outFile << "Invalid move name: " << wordOne << std::endl << std::endl;
 		outFile.close();
 		return;
 	}
-	std::string *move = std::find(std::begin(move_names), std::end(move_names), wordTwo);
-	if (move == std::end(move_names))
-		outFile << "Invalid move name: " << wordTwo << std::endl;
-	else if (wordTwo == "down-smash")
-		outFile << wordOne << " down-smash: " << (character->second).getDsmashTime() << std::endl;
-	else if (wordTwo == "down-tilt")
-		outFile << wordOne << " down-tilt: " << (character->second).getDtiltTime() << std::endl;
-	else if (wordTwo == "forward-smash")
-		outFile << wordOne << " forward-smash: " << (character->second).getFsmashTime() << std::endl;
-	else if (wordTwo == "forward-tilt")
-		outFile << wordOne << " forward-tilt: " << (character->second).getFtiltTime() << std::endl;
-	else if (wordTwo == "jab")
-		outFile << wordOne << " jab: " << (character->second).getJabTime() << std::endl;
-	else if (wordTwo == "up-smash")
-		outFile << wordOne << " up-smash: " << (character->second).getUsmashTime() << std::endl;
-	else
-		outFile << wordOne << " up-tilt: " << (character->second).getUsmashTime() << std::endl;
+	// Else, go on like normal
+	outFile << "-f " << wordOne << " " << wordTwo << std::endl;
+	std::set<Fighter> fighterSet;
+	// Turn the map into an ordered set
+	for (std::map<std::string, Fighter>::iterator i = data.begin(); 
+			i != data.end(); i++) {
+		(i->second).setMove(wordOne);
+		fighterSet.insert(i->second);
+	}
+	// Loop through the set
+	int count = 0;
+	for (std::set<Fighter>::iterator i = fighterSet.begin(); 
+			i != fighterSet.end(); i++) {
+		// Break out if the number requested has been met
+		if (count == stoi(wordTwo)) {
+			break;
+		}
+		// output the information for that index in the set
+		outFile << i->getName() << " " << i->GetMoveTime() << std::endl;
+		count++;
+	}
+	// Close out of the file
 	outFile << std::endl;
 	outFile.close();
 	return;
 }
-void fAction(char*& outputFile, std::map<std::string, Fighter>& data, std::string& wordOne, std::string& wordTwo) {
+// Code for -s action
+void sAction(char*& outputFile, std::map<std::string, Fighter>& data,
+		std::string& wordOne, std::string& wordTwo) {
+	// Create ofstream for output file
 	std::ofstream outFile;
 	outFile.open(outputFile, std::ios_base::app);
-	outFile << "-f " << wordOne << " " << wordTwo << std::endl;
-	if (wordOne == "down-smash") {
-		auto cmp = [](Fighter a, Fighter b) {return a.getDsmashTime() > b.getDsmashTime();};
-		std::set<Fighter, decltype(cmp)> fighterSet;
-		for (std::map<std::string, Fighter>::iterator i = data.begin(); i != data.end(); i++) {
-			fighterSet.insert(i->second);
+	// Check if the move selected is valid
+	bool validMove = false;
+	for (unsigned int i=0; i < 7; i++) {
+		if (move_names[i] == wordOne) {
+			validMove = true;
 		}
-		int count = 0;
-		for (std::set<Fighter, decltype(cmp)>::iterator i = fighterSet.begin(); i != fighterSet.end(); i++) {
-			if (count == stoi(wordTwo)) {
-				break;
-			}
-			outFile << i->getName() << " " << i->getDsmashTime() << std::endl << std::endl;
-			count++;
-		}
-
 	}
-
+	// If the move is invalid, say so
+	if (!validMove) {
+		outFile << "Invalid move name: " << wordOne << std::endl 
+			<< std::endl;
+		return;
+	}
+	// else, print normally
+	outFile << "-s " << wordOne << " " << wordTwo << std::endl;
+	std::set<Fighter, std::greater<Fighter>> fighterSet;
+	// Put the map into a sorted sort from largest to smallest
+	for (std::map<std::string, Fighter>::iterator i = data.begin();
+			i != data.end(); i++) {
+		(i->second).setMove(wordOne);
+		fighterSet.insert(i->second);
+	}
+	int count = 0;
+	// Loop through the set
+	for (std::set<Fighter>::iterator i = fighterSet.begin(); 
+			i != fighterSet.end(); i++) {
+		// Break out if enough have been printed
+		if (count == stoi(wordTwo)) {
+			break;
+		}
+		// Print the index's data
+		outFile << i->getName() << " " << i->GetMoveTime() << std::endl;
+		count++;
+	}
+	outFile << std::endl;
+	// Close out of the file
+	outFile.close();
 	return;
 }
-void sAction(char*& outputFile, std::map<std::string, Fighter>& data, std::string& wordOne, std::string& wordTwo) {
+// Code for -d action
+void dAction(char*& outputFile, std::map<std::string, Fighter>& data, 
+		std::string& wordOne, std::string& wordTwo) {
+	// Create an ofstream for the output file
+	std::ofstream outFile;
+	outFile.open(outputFile, std::ios_base::app);
+	// Check if it is a valid move
+	bool validMove = false;
+	for (unsigned int i=0; i < 7; i++) {
+		if (move_names[i] == wordOne) {
+			validMove = true;
+		}
+	}
+	// If not valid move, say so
+	if (!validMove) {
+		outFile << "Invalid move name: " << wordOne << std::endl << std::endl;
+		return;
+	}
+	// else, continue like normal
+	outFile << "-d " << wordOne << " " << wordTwo << std::endl;
+	std::set<Fighter> fighterSet;
+	// Insert the map into a set from lowest to largest
+	for (std::map<std::string, Fighter>::iterator i = data.begin(); 
+			i != data.end(); i++) {
+		(i->second).setMove(wordOne);
+		fighterSet.insert(i->second);
+	}
+	// iterate through the set, and print out every index with the requested number
+	bool foundNum = false;
+	for (std::set<Fighter>::iterator i = fighterSet.begin(); 
+			i != fighterSet.end(); i++) {
+		if (i->GetMoveTime() == stoi(wordTwo)) {
+			outFile << i->getName() << std::endl;
+		}
+		if (foundNum && i->GetMoveTime() != stoi(wordTwo))
+			break;
+	}
+	outFile << std::endl;
+	outFile.close();
 	return;
 }
-void dAction(char*& outputFile, std::map<std::string, Fighter>& data, std::string& wordOne, std::string& wordTwo) {
-	return;
-}
+// Parse through the database of fighter data
 std::map<std::string, Fighter> ParseDatabase(char*& inputFileName) {
+	// Create a map of strings and fighters
 	std::map<std::string, Fighter> fighterMap;
+	// Open up the database file
 	std::ifstream inFile;
 	inFile.open(inputFileName);
+	// Create initial variables
 	std::string currentWord;
 	std::string name;
 	int jab;
@@ -99,8 +208,10 @@ std::map<std::string, Fighter> ParseDatabase(char*& inputFileName) {
 	int dsmash;
 	bool isCharacter = true;
 	unsigned int wordNum = 0;
+	// Loop through every word
 	if (inFile.is_open()) {
 		while(inFile >> currentWord) {
+			// Go through each word on the line, settings different variables
 			switch(wordNum) {
 				case 0:
 					name = currentWord;
@@ -153,8 +264,11 @@ std::map<std::string, Fighter> ParseDatabase(char*& inputFileName) {
 					} else {
 						isCharacter = false;
 					}
+					// If it is a character (not first line)
 					if (isCharacter)
-						fighterMap.insert({name,Fighter(name, jab, ftilt, utilt, dtilt, fsmash, usmash, dsmash)});
+						// Insert the data into the map
+						fighterMap.insert({name,Fighter(name, jab, ftilt, 
+									utilt, dtilt, fsmash, usmash, dsmash)});
 					wordNum = -1;
 					isCharacter = true;
 					break;
@@ -166,18 +280,25 @@ std::map<std::string, Fighter> ParseDatabase(char*& inputFileName) {
 			wordNum++;
 		}
 	}
+	// Return the map
 	return fighterMap;
 }
-void ParseInput(char*& inputFile, char*& outputFile, std::map<std::string, Fighter>& data) {
+// Parses the lines from the input file
+void ParseInput(char*& inputFile, char*& outputFile, 
+		std::map<std::string, Fighter>& data) {
+	// Create ifstream for input file
 	std::ifstream inFile;
 	inFile.open(inputFile);
+	// Create initial variables
 	std::string currentWord;
 	std::string wordOne;
 	std::string wordTwo;
 	char action;
 	unsigned int num;
+	// Loop through each word
 	if (inFile.is_open()) {
 		while(inFile >> currentWord) {
+			// Get the current action
 			if (currentWord == "-q") {
 				action = 'q';
 				num = 0;
@@ -191,9 +312,12 @@ void ParseInput(char*& inputFile, char*& outputFile, std::map<std::string, Fight
 				action = 'd';
 				num = 0;
 			} else if (num == 1)
+				// Get wordOne
 				wordOne = currentWord;
 			else if (num == 2) {
+				// Get wordTwo
 				wordTwo = currentWord;
+				// Call the function basedo on the action, wordOne, and wordTwo
 				if (action == 'q')
 					qAction(outputFile, data, wordOne, wordTwo);
 				else if (action == 'f') 
@@ -208,28 +332,35 @@ void ParseInput(char*& inputFile, char*& outputFile, std::map<std::string, Fight
 	}
 	return;
 }
+// Main code that runs at the start of the program
 int main(int argc, char** argv){
-	// Argument parsing
+	// Check if the proper number of arguments is enttered
 	if(argc != 4){
-		std::cerr << "Proper usage is " << argv[0] << " [database file] [input file] [output file]" << std::endl;
+		std::cerr << "Proper usage is " << argv[0] << 
+			" [database file] [input file] [output file]" << std::endl;
 		return -1;
 	}
-
+	// Check if the database can be opened
 	std::ifstream dbfile(argv[1]);
 	if(!dbfile){
-		std::cerr << "Failed to open database " << argv[1] << " for reading." << std::endl;
+		std::cerr << "Failed to open database " << argv[1] << 
+			" for reading." << std::endl;
 	}
-
+	// Check if the input file can be opened
 	std::ifstream infile(argv[2]);
 	if(!infile){
-		std::cerr << "Failed to open input " << argv[2] << " for reading." << std::endl;
+		std::cerr << "Failed to open input " << argv[2] << 
+			" for reading." << std::endl;
 	}
-
+	// Check if the output file can be opened
 	std::ofstream outfile(argv[3]);
 	if(!outfile){
-		std::cerr << "Failed to open output " << argv[3] << " for writing." << std::endl;
+		std::cerr << "Failed to open output " << argv[3] 
+			<< " for writing." << std::endl;
 	}
+	// Call ParseDatabase
 	std::map<std::string, Fighter> data = ParseDatabase(argv[1]);
+	// Call ParseInput with the map
 	ParseInput(argv[2], argv[3], data);
 	return 1;
 }
