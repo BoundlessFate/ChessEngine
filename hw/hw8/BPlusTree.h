0b6534f1446830dc5397577cadcf41d298cfd701
Christopher Villanueva <villac3@rpi.edu>
2023-03-30 14:54:45 -0400

    Completed Homework8

--- hw/hw8/BPlusTree.h
+++ hw/hw8/BPlusTree.h
@@ -86,7 +86,9 @@ class BPlusTree{
 		BPlusTreeNode<T>* find(const T& key);
 		void print_sideways(std::ofstream& outfile);
 		void print_BFS(std::ofstream& outfile);
+		// Returns b
 		int GetB() {return b;}
+		// Returns the root node
 		BPlusTreeNode<T>* GetRootNode() {return rootNode;}
 	private:
 		BPlusTreeNode<T>* rootNode;
@@ -95,21 +97,27 @@ class BPlusTree{
 		void split(BPlusTreeNode<T>* node);
 		BPlusTreeNode<T>* find(const T key, BPlusTreeNode<T>* node);
 		void print_sideways(short depth, BPlusTreeNode<T>* node, std::ofstream& outfile);
-		void print_BFS(short depth, short int targetDepth, BPlusTreeNode<T>* node, std::ofstream& outfile);
+		void print_BFS(short depth, short int targetDepth,
+				BPlusTreeNode<T>* node, std::ofstream& outfile, bool& firstOnLine);
 };
+// Main constructor
 template <class T>
 BPlusTree<T>::BPlusTree(const int& num) {
 	rootNode = NULL;
+	// Set the b value
 	b = num;
 	return;
 }
+// Copy Constructor
 template <class T>
 BPlusTree<T>::BPlusTree(BPlusTree& a) {
 	rootNode = NULL;
+	// Set the b value from the other tree
 	b = a.GetB();
 	CopyHelper(a.GetRootNode());
 	return;
 }
+// Helper function for the copy constructor
 template <class T>
 void BPlusTree<T>::CopyHelper(BPlusTreeNode<T>* node) {
 	// Insert the keys if they are a leaf node
@@ -125,16 +133,20 @@ void BPlusTree<T>::CopyHelper(BPlusTreeNode<T>* node) {
 	}
 	return;
 }
+// Insert key into nodes
 template<class T>
 void BPlusTree<T>::insert(T key) {
+	// If the tree is empty, add key to a new root node
 	if (rootNode == NULL) {
 		rootNode = new BPlusTreeNode<T>();
 		(*rootNode).keys.push_back(key);
 		return;
 	}
+	// Call the recursive helper function
 	insert(key, rootNode);
 	return;
 }
+// Helper function for insert
 template <class T>
 void BPlusTree<T>::insert(T key, BPlusTreeNode<T>* node) {
 	// If you are at the bottom of the branch
@@ -148,11 +160,13 @@ void BPlusTree<T>::insert(T key, BPlusTreeNode<T>* node) {
 				break;
 			}
 		}
+		// Insert it at the end of the key vector if it is the greatest value
 		if (!inserted) {
 			(*node).keys.push_back(key);
 		}
 		// If you arent at the bottom node, traverse downward
 	} else {
+		// Recurse to the proper child
 		bool lastIndex = true;
 		for (unsigned int i=0; i<(*node).keys.size(); i++) {
 			if (key < (*node).keys[i]) {
@@ -161,6 +175,7 @@ void BPlusTree<T>::insert(T key, BPlusTreeNode<T>* node) {
 				break;
 			}
 		}
+		// Recurse to the last child if it not in
 		if (lastIndex)
 			insert(key, (*node).children[(*node).keys.size()]);
 	}
@@ -169,14 +184,18 @@ void BPlusTree<T>::insert(T key, BPlusTreeNode<T>* node) {
 		split(node);
 	return;
 }
-
+// Split if the number of children is greater than b
 template <class T>
 void BPlusTree<T>::split(BPlusTreeNode<T>* node) {
+	// If the root node needed to be split
 	if ((*node).parent == NULL) {
+		// Create a new parent node
 		(*node).parent = new BPlusTreeNode<T>;
+		// Set up the parenting and children settings
 		rootNode = (*node).parent;
 		(*node).parent->children.push_back(node);
 	}
+	// Get a reference to parentNode for ease of use
 	BPlusTreeNode<T>* parentNode = (*node).parent;
 	// Get index of currentNode in parent node
 	int index = 0;
@@ -187,15 +206,22 @@ void BPlusTree<T>::split(BPlusTreeNode<T>* node) {
 	}
 	// Create the new node
 	BPlusTreeNode<T>* newNode = new BPlusTreeNode<T>();
+	// Set its parent to the same parent as the current node
 	(*newNode).parent = parentNode;
+	// Add this new node 1 spot after the old one in the parents children vector
 	(*parentNode).children.insert((*parentNode).children.begin()+index+1, newNode);
+	// current node keys / 2
 	int tempNum = (int)((*node).keys.size() / 2);
+	// Iterate backwards through the current nodes keys
 	for (int i=(*node).keys.size() - 1; i>=tempNum; i--) {
+		// Add said key to the new node
 		(*newNode).keys.insert((*newNode).keys.begin(), (*node).keys[i]);
+		// Erase said key from the old node
 		(*node).keys.erase((*node).keys.begin() + i);
 	}
 	bool inserted = false;
 	T key = (*newNode).keys[0];
+	// Insert the first key on the new node to the parent
 	for (unsigned int i=0; i<(*parentNode).keys.size(); i++) {
 		if (key < (*parentNode).keys[i]) {
 			(*parentNode).keys.insert((*parentNode).keys.begin()+i, key);
@@ -205,7 +231,7 @@ void BPlusTree<T>::split(BPlusTreeNode<T>* node) {
 	}
 	if (!inserted)
 		(*parentNode).keys.push_back(key);
-	// Edit children for newNode and node
+	// Repeat the same process with the keys with the children
 	if ((*node).children.size() > 0) {
 		tempNum = (int)((*node).children.size() / 2);
 		for (int i=(*node).children.size() - 1; i>=tempNum; i--) {
@@ -220,36 +246,46 @@ void BPlusTree<T>::split(BPlusTreeNode<T>* node) {
 	}
 	return;
 }
-
+// Deconstructor
 template <class T>
 BPlusTree<T>::~BPlusTree() {
+	// Dont do anything with an empty tree
 	if (rootNode == NULL)
 		return;
+	// Call helper function
 	DeleteNode(rootNode);
 	return;
 }
-
+// Recursive helper for the deconstructor
 template <class T>
 void BPlusTree<T>::DeleteNode(BPlusTreeNode<T>* node) {
+	// Loops through and recurses to the children
 	for (unsigned int i=0; i<(*node).children.size(); i++) {
 		DeleteNode((*node).children[i]);
 	}
+	// Clear the children out of the vector
 	(*node).children.clear();
+	// If it is a leaf node
 	if ((*node).is_leaf()) {
+		// Delete the node
 		delete node;
 		return;
 	}
 	return;
 }
-
+// Find function to find the node with the requested key
 template <class T>
 BPlusTreeNode<T>* BPlusTree<T>::find(const T& key) {
+	// return null if it is a empty tree
 	if (rootNode == NULL)
 		return NULL;
+	// Call the recursive helper function
 	return find(key, rootNode);
 }
+// Recursive function for find
 template <class T>
 BPlusTreeNode<T>* BPlusTree<T>::find(T key, BPlusTreeNode<T>* node) {
+	// Return the node if it is a leaf
 	if ((*node).is_leaf())
 		return node;
 	// If it is not a leaf, recurse further down
@@ -260,15 +296,21 @@ BPlusTreeNode<T>* BPlusTree<T>::find(T key, BPlusTreeNode<T>* node) {
 	}
 	return find(key, (*node).children[(*node).keys.size()]);
 }
-
+// Print sideways function to an outfile
 template <class T>
 void BPlusTree<T>::print_sideways(std::ofstream& outfile) {
-	if (rootNode == NULL)
+	// Dont recurse if it is empty 
+	if (rootNode == NULL) {
+		outfile << "Tree is empty." << std::endl;
 		return;
+	}
+	// Call the recursive helper function
 	print_sideways(0, rootNode, outfile);
 }
+// Recursive helper function for print sideways
 template<class T>
 void BPlusTree<T>::print_sideways(short depth, BPlusTreeNode<T>* node, std::ofstream& outfile) {
+	// Print out the keys if it is a leaf
 	if ((*node).is_leaf()) {
 		for (unsigned int i=0; i<(uint)depth; i++)
 			outfile << '\t';
@@ -277,10 +319,13 @@ void BPlusTree<T>::print_sideways(short depth, BPlusTreeNode<T>* node, std::ofst
 			if (i + 1 <(*node).keys.size())
 				outfile << ',';
 		}
+	// If it isnt a leaf
 	} else {
+		// Recurse through the first half of the children
 		for (unsigned int i=0; i<((*node).children.size()/2); i++) {
 			print_sideways(depth+1, (*node).children[i], outfile);
 		}
+		// Print the current node
 		for (unsigned int i=0; i<(uint)depth; i++)
 			outfile << '\t';
 		for (unsigned int i=0; i<(*node).keys.size(); i++) {
@@ -289,17 +334,23 @@ void BPlusTree<T>::print_sideways(short depth, BPlusTreeNode<T>* node, std::ofst
 				outfile << ',';
 		}
 		outfile << std::endl;
+		// Recurse through the rest of the children
 		for (unsigned int i=(int)((*node).children.size()/2); i<(*node).children.size(); i++) {
 			print_sideways(depth+1, (*node).children[i], outfile);
 		}
+		return;
 	}
 	outfile<< std::endl;
 	return;
 }
+// Other print function (Breadth First Search)
 template <class T>
 void BPlusTree<T>::print_BFS(std::ofstream& outfile) {
-	if (rootNode == NULL)
+	// Dont recurse if it is an empty tree
+	if (rootNode == NULL) {
+		outfile << "Tree is empty." << std::endl;
 		return;
+	}
 	// Find the max depth
 	short maxDepth = 0;
 	BPlusTreeNode<T>* currentNode = rootNode;
@@ -311,24 +362,32 @@ void BPlusTree<T>::print_BFS(std::ofstream& outfile) {
 	}
 	// Iterate through all levels of the tree
 	for (short i=0; i<=maxDepth; i++) {
-		print_BFS(0, i, rootNode, outfile);
+		bool firstOnLine = true;
+		// Recursive helper function
+		print_BFS(0, i, rootNode, outfile, firstOnLine);
 		outfile << std::endl;
 	}
 }
+// Recursive helper function for print BFS
 template <class T>
-void BPlusTree<T>::print_BFS(short depth, short targetDepth, BPlusTreeNode<T>* node, std::ofstream& outfile) {
+void BPlusTree<T>::print_BFS(short depth, short targetDepth, BPlusTreeNode<T>* node, std::ofstream& outfile, bool& firstOnLine) {
+	// Print out the current keys if it is on the right depth
 	if (depth == targetDepth) {
+		if (!firstOnLine) {
+			outfile << '\t';
+		}
 		// Print the data for the current node
 		for (unsigned int i=0; i<(*node).keys.size(); i++) {
 			outfile << (*node).keys[i];
 			if (i+1<(*node).keys.size())
 				outfile << ',';
 		}
-		outfile << '\t';
+		firstOnLine = false;
 		return;
 	}
+	// If it isnt on the right depth, do down a level in depth and add 1 to depth
 	for (unsigned int i=0; i<(*node).children.size(); i++) {
-		print_BFS(depth + 1, targetDepth, (*node).children[i], outfile);
+		print_BFS(depth + 1, targetDepth, (*node).children[i], outfile, firstOnLine);
 	}
 	return;
 }
--- hw/hw8/README.txt
+++ hw/hw8/README.txt
@@ -1,7 +1,7 @@
 HOMEWORK 8: B+ TREES
 
 
-NAME:  < insert name >
+NAME: Christopher Villanueva
 
 
 COLLABORATORS AND OTHER RESOURCES:
@@ -10,13 +10,13 @@ List the names of everyone you talked to about this assignment
 LMS, etc.), and all of the resources (books, online reference
 material, etc.) you consulted in completing this assignment.
 
-< insert collaborators / resources >
+Joshua Collins
 
 Remember: Your implementation for this assignment must be done on your
 own, as described in "Academic Integrity for Homework" handout.
 
 
-ESTIMATE OF # OF HOURS SPENT ON THIS ASSIGNMENT:  < insert # hours >
+ESTIMATE OF # OF HOURS SPENT ON THIS ASSIGNMENT:  ~10 hours
 
 
 TESTING & DEBUGGING STRATEGY: 
@@ -25,6 +25,14 @@ What tools did you use (gdb/lldb/Visual Studio debugger,
 Valgrind/Dr. Memory, std::cout & print, etc.)?  How did you test the
 "corner cases" of your implementation?
 
+To debug my program, I used gdb to go line by line through my program. 
+I also drew out visual models of my trees to test if it was working properly.
+I used the test file provided as well to see if my program worked, as my code
+passed all the assert statements. For memory issues, I used Dr. Memory to verify 
+I had no memory errors or leaks, and I made sure to add a deconstructor and keep
+track of all my heap memory. To test corner cases, I drew out many test cases
+on paper and went through my code by hand to make sure it would run properly
+on edge cases.
 
 
 
